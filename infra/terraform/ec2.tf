a12d557f479eba234485ec10377cc63a67faf833
marcin.szewczyk <marcin.szewczyk@ocado.com>
2026-01-07 15:48:10 +0100

    terraform fix

--- infra/terraform/ec2.tf
+++ infra/terraform/ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "app" {
   vpc_security_group_ids = [aws_security_group.ec2.id]
 
   root_block_device {
-    volume_size = 20
+    volume_size = 30
     volume_type = "gp3"
   }
 
